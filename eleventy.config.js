972dc4d8148126290b5fc87801b9764fca32412f
vsslctrl <accounts@burmarkelectrical.nz>
2026-06-26 22:44:31 +0200

    SEO

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,14 @@
+import format from 'date-fns/format'
+
 export default function(eleventyConfig) {
+
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("images");
+
+  //XML dates
+  eleventyConfig.addFilter('date', function (date, dateFormat) {
+    return format(date, dateFormat)
+  })
 
   eleventyConfig.addFilter("capitalizeFirst", function(string) {
     if (!string) return string; // Handle empty or null strings
